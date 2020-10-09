d00162fa6c8a8f06faaf3cddbf029aaeab374b8b
edsmarroquin <57919696+edsmarroquin@users.noreply.github.com>
2020-10-09 17:29:51 -0400

    StudentDatabaseApp

---
+++ Student.java
@@ -0,0 +1,17 @@
+package studentDatabaseApp;
+
+public class Student {
+
+	private int numOfStudents;
+	private String name;
+	private int year;
+	private int id;
+	private String course;
+	private int cost;
+	
+	public Student() {
+		
+	}
+	
+	//
+}
---
+++ StudentDatabaseApp.java
@@ -0,0 +1,11 @@
+package studentDatabaseApp;
+
+public class StudentDatabaseApp {
+	public static void main(String[] args) {
+		
+		//Ask how many users we want to add
+		
+		//create n number of new students
+	}
+
+}
